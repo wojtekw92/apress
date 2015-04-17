0b4accabf1c9466ddb1b42bd0e18c50dde564f80
Michał Budzyński <michal@virtualdesign.pl>
2015-04-17 23:24:50 +0200

    lint fixes

--- apress.js
+++ apress.js
@@ -13,21 +13,23 @@ var routeLib = (function(){
   var setup = {
     storeage: false,  //USE storeage api for storeing last route
     rouetingEnable: true, //You can disable router lib
-  }
+  };
+
   var hashTest = function () {
     if(setup.rouetingEnable) {
       var hash = window.location.hash;
       hash = hash.replace('#','');
       for(var i = 0; i< routes.length; i++) {
         var resualt = routes[i].regexp.exec(hash);
-        if(resualt != null) {
+        if(resualt !== null) {
           resualt = resualt.slice(1);
           routes[i].callback.apply(this, resualt);
           break;
         }
       }
     }
-  }
+  };
+
   window.onhashchange = hashTest;
   var addRoute = function(route, callback) {
     var reg = route.replace('/','\\/');
@@ -36,7 +38,8 @@ var routeLib = (function(){
     routes.push({'route': route,
                  'regexp': new RegExp(reg,'i'),
                  'callback': callback});
-  }
+  };
+  
   //API for router
   return {
     addRoute: addRoute,
