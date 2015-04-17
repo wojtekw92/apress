00fde6634dd9a6ca6f499f66ec14c4080cec9326
Michał Budzyński <michal@virtualdesign.pl>
2015-04-17 23:27:30 +0200

    typo fixes

--- apress.js
+++ apress.js
@@ -11,19 +11,19 @@ var routeLib = (function(){
 
   */
   var setup = {
-    storeage: false,  //USE storeage api for storeing last route
-    rouetingEnable: true, //You can disable router lib
+    storage: false,  //USE storage api for storeing last route
+    routingEnable: true, //You can disable router lib
   };
 
   var hashTest = function () {
-    if(setup.rouetingEnable) {
+    if(setup.routingEnable) {
       var hash = window.location.hash;
       hash = hash.replace('#','');
       for(var i = 0; i< routes.length; i++) {
-        var resualt = routes[i].regexp.exec(hash);
-        if(resualt !== null) {
-          resualt = resualt.slice(1);
-          routes[i].callback.apply(this, resualt);
+        var result = routes[i].regexp.exec(hash);
+        if(result !== null) {
+          result = result.slice(1);
+          routes[i].callback.apply(this, result);
           break;
         }
       }
@@ -31,6 +31,7 @@ var routeLib = (function(){
   };
 
   window.onhashchange = hashTest;
+  
   var addRoute = function(route, callback) {
     var reg = route.replace('/','\\/');
     reg = reg.replace('*','[\\w-]+');
@@ -39,7 +40,7 @@ var routeLib = (function(){
                  'regexp': new RegExp(reg,'i'),
                  'callback': callback});
   };
-  
+
   //API for router
   return {
     addRoute: addRoute,
