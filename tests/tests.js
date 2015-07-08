f71f0afb16883d340fa95c231f29652a54974232
Michał Budzyński <michal@virtualdesign.pl>
2015-07-08 02:16:13 +0200

    :satellite: add more tests

--- tests/tests.js
+++ tests/tests.js
@@ -26,7 +26,7 @@ module('Apress simple routing module',{
 
 test('should run proper function for main route (`/`)', function() {
   this.testRoute('/');
-  ok(this.status);
+  ok(this.status, 'proper root listener called');
 });
 
 test('should run proper function for simple route - `/foo`', function() {
@@ -39,7 +39,7 @@ test(
   'end - `/foobar/`',
 function() {
   this.testRoute('/foobar', '/foobar/');
-  ok(this.status);
+  ok(this.status, 'proper root listener called');
 });
 
 test(
@@ -47,44 +47,62 @@ test(
   '`/foo/something` is called',
 function() {
   this.testRoute('/foo', '/foo/bar');
-  ok(!this.status);
+  ok(!this.status, 'root listener wasn\'t called');
 });
 
 test('should run proper function for more complex route - `/foo/bar`',
 function() {
   this.testRoute('/foo/bar');
-  ok(this.status);
+  ok(this.status, 'proper root listener called');
 });
 
 test('should understand wildcards at the end of the routes - `/foo/*`',
 function() {
   this.testRoute('/foo/*', '/foo/barfoo');
-  ok(this.status);
+  ok(this.status, 'proper root listener called');
 });
 
 test('should understand wildcards in the middle of the routes - `/foo/*/bar`',
 function() {
   this.testRoute('/foo/*/bar', '/foo/barfoo/bar');
-  ok(this.status);
+  ok(this.status, 'proper root listener called');
 });
 
 test('should pass proper parameter `/param/%`',
 function() {
   this.testRoute('/param/%', '/param/bar');
-  ok(this.status);
-  equal(this.param, 'bar');
+  ok(this.status, 'proper root listener called');
+  equal(this.param, 'bar', 'proper param passed to the listener');
 });
 
 test('should pass proper parameter in the middle `/qwe/%/yo`',
 function() {
   this.testRoute('/qwe/%/yo', '/qwe/bar/yo');
-  ok(this.status);
-  equal(this.param, 'bar');
+  ok(this.status, 'proper root listener called');
+  equal(this.param, 'bar', 'proper param passed to the listener');
 });
 
 test('should pass proper parameter and understand wildcard `/hi/*/%`',
 function() {
   this.testRoute('/hi/*/%', '/hi/bar/yo');
+  ok(this.status, 'proper root listener called');
+  equal(this.param, 'yo', 'proper param passed to the listener');
+});
+
+test('should understand route defined by RegExp - /[A-Z][0-9]/',
+function() {
+  this.testRoute(/[A-Z][0-9]/, '/B5/');
+  ok(this.status, 'proper root listener called');
+});
+
+test('should run ErrorPage listener if route cannot be found',
+function() {
+  apress.setErrorPage(function(){
+    this.status = true;
+  }.bind(this));
+
+  window.location.hash = '#PeperePutti';
+  apress.hashTest();
+
   ok(this.status);
-  equal(this.param, 'yo');
 });
