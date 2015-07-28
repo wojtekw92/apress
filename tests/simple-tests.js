1c90aceacef236e74f719720a9d1890314937a52
Michał Budzyński <michal@virtualdesign.pl>
2015-07-28 01:01:00 +0100

    :art: remove old tests

--- tests/simple-tests.js
+++ tests/simple-tests.js
@@ -47,27 +47,6 @@ function() {
   ok(!this.status, 'listener wasn\'t called');
 });
 
-test('should call multiple callbacks attached to the same route',
-function() {
-  var status1 = false;
-  var status2 = false;
-  var route = '/foo';
-
-  apress.addRoute(route, function(){
-    status1 = true;
-  });
-
-  apress.addRoute(route, function(){
-    status2 = true;
-  });
-
-  window.location.hash = '#!' + route;
-  apress.hashTest();
-
-  ok(status1, 'first listener fired');
-  ok(status2, 'second listener fired');
-});
-
 test(
 'should run proper function for simple route with slash ant the ' +
   'end - `/foobar/`',
